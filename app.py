f414e0becd4a17cae703a497588b46e6fc27411e
AnimeshPandey123 <ani.mesh13578@gmail.com>
2019-06-14 14:29:32 +0545

    first

---
+++ app.py
@@ -0,0 +1,125 @@
+from flask import Flask, jsonify, render_template
+from check_celery import make_celery
+from learn import *
+
+import json
+
+app = Flask(__name__)
+
+app.config['CELERY_BROKER_URL'] = 'amqp://localhost//'
+app.config['CELERY_RESULT_BACKEND'] = ''
+
+celery = make_celery(app)
+
+stores = [
+	{
+		'name':'My Wonderful Store',
+		'items': [
+			{
+				'name': 'My Item',
+				'price': 15.99
+			}
+		]
+	},
+	{
+		'name':'asd',
+		'items': [
+			{
+				'name': 'sss',
+				'price': 15.99
+			}
+		]
+	}
+]
+
+@app.route('/process/<name>')
+def process(name):
+	reverse.apply_async(args=[name], countdown=3)
+	return 'I sent an async request!'
+
+@celery.task(name='app.reverse')
+def reverse(string):
+	f = open("mytext.txt", "a")
+	f.write(string[::-1])
+	f.close()
+	return string[::-1]
+
+@app.route('/')
+def home():
+	f = open("mytext.txt", "a")
+	f.write("Now the file has more content!")
+	f.close()
+	return render_template('index.html')
+
+@app.route('/training')
+def training():
+	learn.apply_async(countdown=3)
+	return jsonify({'data': 'tarining'})
+
+@celery.task(name='app.learn')
+def learn():
+	f = TrainModel()
+	k = f.train()
+	f.withImage()
+	f.saveModel()
+	return 'Completed'
+
+@app.route('/getEpoch')
+def tragetEpochining():
+	f = open("log.txt", "r")
+	return f.read()
+
+@app.route('/test')
+def test():
+	f =  TrainModel()
+	f.loadModel()
+	pred = f.predictImage()
+	return pred
+
+
+# # POST /store data: {name:}
+# @app.route('/store', methods=['POST'])
+# def create_store():
+# 	request_data = request.get_json()
+# 	new_store = {
+# 		'name': request_data['name'],
+# 		'items': []
+# 	}
+# 	stores.append(new_store)
+# 	return jsonify(new_store)
+
+# @app.route('/store/<string:name>')
+# def get_store(name):
+# 	# Iterate over stores
+# 	for i in stores:
+# 		if i['name'] == name:
+# 			return jsonify({'item':i});
+
+# 	return jsonify({'error': 'Not Found'});
+
+# @app.route('/store/')
+# def get_stores():
+# 	return jsonify({'stores': stores})
+
+# @app.route('/store/<string:name>/item', methods=['POST'])
+# def create_item_in_store(name):
+# 	request_data = request.get_json()
+# 	new_item = {
+# 		'name': request_data['name'],
+# 		'price': request_data['price']
+# 	}
+# 	for i in stores:
+# 		if i['name'] == name:
+# 			i['items'].append(new_item)
+# 			return jsonify({'items': i});
+# 	return jsonify({'error': 'Not Found'});
+
+# @app.route('/store/<string:name>/item', methods=['GET'])
+# def get_items_in_store(name):
+# 	for i in stores:
+# 		if i['name'] == name:
+# 			return jsonify({'items':i['items']});
+# 	return jsonify({'error': 'Not Found'});
+
+if __name__ == '__main__':
+	app.run()
---
+++ learn.py
@@ -0,0 +1,108 @@
+# Convolutional Neural Network
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import keras as keras
+from keras.preprocessing import image
+from keras.models import load_model
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+class TrainModel():
+    """docstring for ClassName"""
+    classifier = Sequential()
+
+    def train(self):
+        self.classifier = Sequential()
+
+        # Step 1 - Convolution
+        self.classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+        # Step 2 - Pooling
+        self.classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+        # Adding a second convolutional layer
+        self.classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+        self.classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+        # Step 3 - Flattening
+        self.classifier.add(Flatten())
+
+        # Step 4 - Full connection
+        self.classifier.add(Dense(units = 128, activation = 'relu'))
+        self.classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+        # Compiling the CNN
+        self.classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+        return self.classifier
+
+
+    def withImage(self):
+        train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+        test_datagen = ImageDataGenerator(rescale = 1./255)
+
+        training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                         target_size = (64, 64),
+                                                         batch_size = 32,
+                                                         class_mode = 'binary')
+
+        test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                                    target_size = (64, 64),
+                                                    batch_size = 32,
+                                                    class_mode = 'binary')
+        self.classifier.fit_generator(training_set,
+                         steps_per_epoch = 50,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 500,
+                         callbacks= [AfterEpoch()]
+                         )
+        return self.classifier
+
+    def predictImage(self):
+        test_image = image.load_img('dataset/single_prediction/cat.jpg', target_size = (64, 64))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = self.classifier.predict(test_image)
+        resultProb = self.classifier.predict_proba(test_image)
+        # training_set.class_indices
+        # print(result)
+        # return result
+        
+        if result[0][0] == 1:
+            prediction = 'dog'
+        else:
+            prediction = 'cat'
+        return prediction
+
+
+    def saveModel(self):
+        self.classifier.save('newer.h5')
+        return self.classifier
+
+    def loadModel(self):
+        keras.backend.clear_session()
+        self.classifier = None;
+        self.classifier = load_model('newer.h5')
+        return self.classifier
+
+
+class AfterEpoch(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        with open('log.txt', 'a+') as f:
+            f.write('%02d %.3f\n' % (epoch, logs['loss']))
